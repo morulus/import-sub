9dd2d549ad1a1c1b60dea136e0062911860d5997
Vladimir Kalmykov <v.kalmykov@infotech.team>
2017-03-09 00:54:17 +0300

    initial alfa.0

--- README.md
+++ README.md
@@ -1,2 +1,176 @@
-# import-sub
-Resolver for import-sub
+Import substitution (alfa)
+==
+
+Technique of customizing, which is based on import substitution.
+
+# Theory
+
+Es6 modules are not supports the dependencies injections. But we still can to substitute several parts of module at the assembly stage by customizing path resolver. If target module requires file or package, it can be substituted to something else.
+
+Operating variables
+---
+
+The process of resolving a path depends on the several values. These values are basic in the process of matching the case and forming a path.
+
+### `request`
+Is the string that user specified in quotation marks. For example:
+```js
+import './bar.js';
+```
+Where `./bar.js` is our **request**.
+
+It can be matched by regular expression `\/bar\.js$/`.
+
+---
+
+### `base`
+The directory from which the request is made. For example:
+
+*The file `src/foo/index.js` requires `./bar.js`.*
+
+Where `src/foo` is our **base**.
+
+It can be strict matched by regular expression `src\/foo`;
+
+---
+
+### `basename` (placeholder)
+if `src/foo` is **base**, then `foo` is **basename**.
+
+---
+
+### `root` (placeholder)
+The root directory of the project. In typical cases `root` is `process.cwd()`, but can be specified in options.
+
+---
+
+### `id` (placeholder)
+Requested filename. If `request` is `./bar.js`, then `id` is `bar.js`.
+
+## Rules
+
+The replacement rule is shaped from two regular expressions for `request` and `base`, wrapped to `match` property and one path-determining property - `use`, which contains new `request` and/or `base`.
+
+```js
+{
+  match: {
+    request: RegEx,
+    base: RegEx
+  },
+  use: {
+    request: string,
+    base: string
+  }
+}
+```
+
+By matching two values (`request` and `base`) you can determine exactly what file is requested and where from, and the concrete case of the customization.
+
+If `request` and `base` are matched to defined value by engine, then instead of origin `request` and `base` it will use same properties from property `use`.
+
+
+### Examples:
+
+We need to substitute required file `./bar.js` from `src/foo` to `../../custom/foo/bar.js`.
+
+```js
+{
+  match: {
+    request: /\.\/bar\.js$/,
+    base: /src\/foo$/
+  },
+  use: {
+    request: '../../custom/foo/bar.js'
+  },
+}
+```
+
+Another way. Using custom `base`.
+
+```js
+{
+  match: {
+    request: /\.\/bar\.js$/,
+    base: /src\/foo$/
+  },
+  use: {
+    base: '<root>/custom/foo'
+  },
+}
+```
+---
+
+Same situation, but for all modules in `src` folder.
+
+```js
+{
+  match: {
+    request: /\.\/bar\.js$/,
+    base: /src\/[^\/]$/
+  },
+  use: {
+    request: '../../custom/<basename>/bar.js',
+  },
+}
+```
+Here we use the placeholder `<basename>`, which in this case will have value `foo`.
+
+---
+
+Replace all `.js` to `.jsx`.
+
+```js
+{
+  match: {
+    request: /\.js$/
+  },
+  use: {
+    request: /\.jsx$/
+  }
+}
+```
+
+### Custom placeholders
+
+For more flexible compilation of the path, you can determine the placeholders by yourself. To do that use round brackets in regular expression.
+
+```js
+{
+  match: {
+    request: /\.\/([\w])\.js$/i
+  },
+  ...
+}
+```
+
+Then the custom placeholder will get the hash `<request:1>`. If there were more groups, then they would be hashed with the index number `<request:2>`, `<request:3>`, etc.
+
+```js
+{
+  ...,
+  use: {
+    request: './<request:0>.jsx'
+  }
+}
+```
+
+You probably already guessed that such capturing groups in the `base` will create the placeholders `<base:1>`, `<base:2>`, etc.
+
+# Benefits
+
+In the field of customization of styles, the method provides a whole set of advantages.
+
+![Compare style customizing ways](./assets/compare-customizing-ways.png)
+
+# Author
+
+Vladimir Kalmykov
+
+<vladimirmorulus@gmail.com>
+
+https://morulus.github.io/
+
+# Implementations
+
+- [postcss-import-sub](https://github.com/morulus/postcss-import-sub)
+- [webpack-import-sub-plugin](https://github.com/morulus/webpack-import-sub-plugin)
---
+++ resolve.js
@@ -0,0 +1,318 @@
+var fs = require('fs');
+var path = require('path');
+var resolve = require('resolve');
+var path = require('path');
+const defaultExpr = /.*/i;
+/**
+* Kick empty sufix
+*/
+function kickEmptySufix(segment, index, all) {
+  return index !== all.length - 1 || segment;
+}
+/**
+* - Replace slashes to unix style
+* - Remove trailing slash.
+*/
+function sepToUnix(p) {
+  return p
+  .split(path.sep)
+  .filter(kickEmptySufix)
+  .join('/');
+}
+/**
+* Test is file exists
+*/
+function testFileExists(filename) {
+  return fs.existsSync(filename[0]);
+}
+/**
+* Make path with dot if it hasnt. This function assumes in advance that you giving to it a relative path.
+*/
+function forceRelative(p) {
+  if (p.charAt(0) !== '.') {
+    return p.charAt(0) === '/' ? p : './'+p;
+  }
+  return p;
+}
+/**
+* Simple replace string with key:value in placeholders
+*/
+function replace(str, placeholders) {
+  for (let prop in placeholders) {
+    if (placeholders.hasOwnProperty(prop)) {
+      str = str.replace(prop, placeholders[prop]);
+    }
+  }
+  return str;
+}
+/**
+* Resolve path the same way ans postcss-import
+*
+* @return {Promise}
+*/
+function resolveAsync(p, base) {
+  if (!path.isAbsolute(p)) {
+    return new Promise(function(r, j) {
+      resolve(forceRelative(p), {
+        basedir: base
+      }, function(err, res) {
+        if (err) {
+          j(err);
+        } else {
+          r(res);
+        }
+      });
+    });
+
+  } else {
+
+    return Promise.resolve(p);
+  }
+}
+/**
+* Simple resolve path (usign path.resolve)
+*/
+function resolveSync(p, base) {
+  if (!path.isAbsolute(p)) {
+    p = path.resolve(base, p);
+  }
+  return p;
+}
+/**
+* Object fn surrogate
+*/
+function object(pairs) {
+  let obj = {};
+  for (let i = 0;i<pairs.length;++i) {
+    obj[pairs[i][0]] = pairs[i][1];
+  }
+  return obj;
+}
+/**
+* Transform regExpr exec result to hashmap with tagged keys
+*/
+function matchToHolders(match, tag) {
+  return object(match.map(function(val, index) {
+    return ["<"+tag+":$"+index+">", val];
+  }));
+}
+/**
+* Filter rules with `to` prop
+*/
+function filterValidRule(rule) {
+  return (rule.hasOwnProperty('match')&&typeof rule.match === "object")
+  && (rule.hasOwnProperty('use')&&typeof rule.use === "object")
+  && (
+    (rule.match.hasOwnProperty('request'))
+    || (rule.match.hasOwnProperty('base'))
+  )
+}
+/**
+* Merge with defaults
+*/
+function mapDefaults(rule) {
+  return {
+    match: Object.assign({
+      id: defaultExpr,
+      base: defaultExpr,
+      module: defaultExpr
+    }, rule.match),
+    use: rule.use,
+  };
+}
+/**
+* Select only rules witj extend property
+*/
+function ruleCheckAppend(rule) {
+  return Object.prototype.hasOwnProperty.call(rule, 'append')&&rule.append;
+}
+/**
+* Search module in rules existing
+*/
+function isRequiresEarlyResolve(rules) {
+  for (let i = 0; i < rules.length; ++i) {
+    if (
+      Object.prototype.hasOwnProperty.call(rules[i], 'module') ||
+      (Object.prototype.hasOwnProperty.call(rules[i], 'append') && rules[i].append)
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
+/**
+* Pick first value from Array<Array>
+*/
+function pickFirst(item) {
+  return item[0];
+}
+/**
+* Pick second value from Array<Array>
+*/
+function pickSecond(item) {
+  return item[1];
+}
+
+/**
+ * Resolve id with simplify logic
+ */
+function defaultResolver(id, base) {
+  return path.resolve(base, id);
+}
+module.exports = function resolveSub(rules, options) {
+  /**
+   * `importOptions`
+   * Backward compatibility with postcss-import
+   */
+  const importOptions = options.importOptions || {};
+  /**
+   * `isModuleRequired`
+   * If `match` contains property `module` or `append` option is enabled,
+   * then no matter what we have to resolve path before.
+   */
+  const isModuleRequired = isRequiresEarlyResolve(rules);
+  /**
+   * `originalResolve`
+   * Options can accept original resolve function (which used by engine).
+   */
+  const originalResolve = typeof options.originalResolve === 'function' ? options.originalResolve : defaultResolver;
+  /**
+   * `root`
+   */
+  const root = options.root || process.cwd();
+  /**
+   * `request`
+   */
+  const request = options.request;
+  /**
+   * `Base`
+   *
+   * Transforms:
+   * - Replace window separate style to unix
+   * - Remove trailing slash
+   */
+  const base = sepToUnix(options.base);
+  /**
+   * `userResolve`
+   * Custom user resolve function.
+   */
+  const userResolve = options.resolve;
+  /**
+   * `basename`
+   */
+  const basename = path.parse(base).base.split('/').pop();
+  /**
+   * Placeholders
+   */
+  const placeholders = {
+    "~": root,
+    "<request>": request,
+    "<root>": root,
+    "<base>": base,
+    "<id>": path.parse(request).name,
+    "<basename>": basename,
+  };
+  /**
+   * For best performance resolve module right now (only if needed)
+   */
+  return (isModuleRequired ? originalResolve(request, base, importOptions) : Promise.resolve(''))
+  .then(function(module) {
+    /**
+     * Flow rules
+     */
+    return [rules
+    /**
+     * We want only rules with `to` or `path` prop.
+     */
+    .filter(filterValidRule)
+    /**
+    * Merge with default expressions (id, base, module)
+    */
+    .map(mapDefaults)
+    /*
+    * Test each expression
+    */
+    .filter(function(rule) {
+      return rule.match.request.test(request)
+      && rule.match.base.test(base)
+      && rule.match.module.test(module);
+    }), module];
+  })
+  .then(function(parcle) {
+    return Promise.all(parcle[0].map(function(rule) {
+      let resolver;
+      /**
+       * Match id
+       */
+      let matchRequest = rule.match.request.exec(request);
+      if (matchRequest) {
+        Object.assign(placeholders, matchToHolders(matchRequest, 'request'));
+      }
+      /**
+       * Match base
+       */
+      let matchBase = rule.match.base.exec(base);
+      if (matchBase) {
+        Object.assign(placeholders, matchToHolders(matchBase, 'base'));
+      }
+      /**
+       * Match module (optional)
+       */
+      if (isModuleRequired) {
+        let matchModule = rule.match.module.exec(parcle[1]);
+        if (matchModule) {
+          Object.assign(placeholders, matchToHolders(matchModule, 'module'));
+        }
+      }
+      /**
+       * Parse aliases and resolve final path
+       */
+      const customRequest = rule.use.request
+        ? replace(rule.use.request, placeholders)
+        : request;
+      const customBase = rule.use.base
+        ? replace(rule.path, placeholders)
+        : base;
+      return resolveAsync(
+        customRequest,
+        customBase !== base ? resolveSync(customBase, base) : customBase
+      )
+      .then(function(file) {
+        return [file, rule];
+      });
+    }))
+    .then(function(files) {
+      return [files, parcle[1]];
+    });
+  })
+  .then(function(parcle) {
+    const files = parcle[0];
+    const module = parcle[1];
+    const existsFiles = files.filter(testFileExists);
+    /**
+    * If at least one of file exists, we returns them
+    */
+    if (existsFiles.length>0) {
+      const isAppendModule = (
+        existsFiles
+        .map(pickSecond)
+        .filter(ruleCheckAppend).length>0
+      );
+      return (isAppendModule ? [module] : []).concat(existsFiles.map(pickFirst));
+    }
+    /**
+    * On fail we must check for use resolve function to execute it
+    */
+    if (userResolve) {
+      return userResolve(request, base, importOptions);
+    }
+    /**
+    * On total fail just returns id, original engine will decide what to do.
+    */
+    return request;
+  })
+  .catch(function(e) {
+    console.log(e);
+    return request;
+  });
+}
